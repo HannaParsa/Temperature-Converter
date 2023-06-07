2b2068af6a485d8ec488fdb2cf2b31a8fdf56dbd
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-06-07 23:04:41 +0330

    in progress

--- converter.py
+++ converter.py
@@ -2,3 +2,11 @@ import tkinter as tk
 from tkinter import messagebox
 from functools import partial
 
+# Declaration of global variable
+temp_Val = "Celsius"
+
+# getting drop down value
+def store_temp(set_temp):
+    global temp_Val
+    temp_Val = set_temp
+
