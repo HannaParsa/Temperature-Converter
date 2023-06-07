50ffca082d43a4baa618e1ce2055bd1f54798ddd
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-06-07 23:05:31 +0330

    in progress

--- converter.py
+++ converter.py
@@ -10,3 +10,24 @@ def store_temp(set_temp):
     global temp_Val
     temp_Val = set_temp
 
+def call_convert(rlabel1,  inputn):
+    temp = inputn.get()
+     
+    if temp_Val == 'Celsius':
+         
+        # Conversion of celsius temperature to fahrenheit
+        f = float((float(temp) * 9 / 5) + 32)
+        rlabel1.config(text ="%.1f Fahrenheit" % f)
+        messagebox.showinfo("Temperature Converter",
+                            "Successfully converted to Fahrenheit ", )
+         
+    if temp_Val == 'Fahrenheit':
+         
+        # Conversion of fahrenheit temperature
+        # to celsius
+        c = float((float(temp) - 32) * 5 / 9)
+        rlabel1.config(text ="%.1f Celsius" % c)
+        messagebox.showinfo("Temperature Converter",
+                            "Successfully converted to Celsius ")
+    return
+
